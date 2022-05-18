e524b7e57a8f04c37db6d0260a1543ece378afa8
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-18 20:59:11 -0300

    exercicio de IF e Else

--- src/curso/executavel/PrimeiraClasseJava.java
+++ src/curso/executavel/PrimeiraClasseJava.java
@@ -4,13 +4,23 @@ public class PrimeiraClasseJava {
 	
 	/* Main é um método auto executável em Java*/
 	public static void main(String[] args) {
-		System.out.println("Minha primeira Classe funcionando!!");
+		System.out.println("IF e Else");
 		
-		int num = 10;
-		int num1 = 20;
+		int num = 7;
+		int num1 = 150;
+		int soma = num+num1;
+		
+		
+		if(soma <= 100) {
+			System.out.println("O valor está entre 0 e 100");
+		}else if((soma >= 100)&& ((soma <= 200))){
+			System.out.println("A soma está entre 100 e 200");
+		}else {
+			System.out.println("A soma é maior que 300");
+		}
 		
 		System.out.println("Soma de valores inteiros abaixo!!");
-		System.out.println(num+num1);
+		System.out.println(soma);
 	}
 	
 }
