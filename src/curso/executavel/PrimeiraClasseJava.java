b49cb72c5e9edebff51a1d86405bbe1e2e14d493
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-18 20:26:18 -0300

    exemplos em Java

---
+++ src/curso/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,10 @@
+package curso.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/* Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		System.out.println("Minha primeira Classe funcionando!!");
+	}
+	
+}
