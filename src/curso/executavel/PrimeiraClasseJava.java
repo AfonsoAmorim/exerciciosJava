fcee5e367ba5e26de8adddc37729c4fa6884e4ea
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-18 20:41:48 -0300

    soma de valores

--- src/curso/executavel/PrimeiraClasseJava.java
+++ src/curso/executavel/PrimeiraClasseJava.java
@@ -5,6 +5,12 @@ public class PrimeiraClasseJava {
 	/* Main é um método auto executável em Java*/
 	public static void main(String[] args) {
 		System.out.println("Minha primeira Classe funcionando!!");
+		
+		int num = 10;
+		int num1 = 20;
+		
+		System.out.println("Soma de valores inteiros abaixo!!");
+		System.out.println(num+num1);
 	}
 	
 }
