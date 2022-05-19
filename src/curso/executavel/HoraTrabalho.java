f69aea2bc7d9e80af8e9181d2decf34af844b06e
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-19 18:29:16 -0300

    novo calculo

---
+++ src/curso/executavel/HoraTrabalho.java
@@ -0,0 +1,32 @@
+package curso.executavel;
+
+import javax.swing.JOptionPane;
+
+public class HoraTrabalho {
+
+	/* Main é um método auto executável em Java */
+	public static void main(String[] args) {
+
+		System.out.println("Digite qual carga de horas acordada no contrato.");
+
+		double horasTrabalhadas = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de horas: "));
+		double salario = Integer.parseInt(JOptionPane.showInputDialog("Digite seu salário: "));
+
+		double pisoSalarial = 1800;
+		double pisoSalarial2 = 3500;
+
+		double valorHora = salario / horasTrabalhadas;
+		if (valorHora <= 100 && salario <= pisoSalarial) {
+			System.out.println("Você tem a mesma renda que 70% da população brasileira: " + salario);
+			System.out.println("O valor da sua hora de trabalho está entre 0 e R$ 100.");
+
+		} else if(valorHora > 100 || salario >= pisoSalarial2){
+			System.out.println("Você tem uma renda maior que 90% da população brasileira: " + salario);
+			System.out.println("O valor da sua hora é maior que R$ 100.");
+		}
+			
+		
+		System.out.printf("A sua hora vale R$ %.2f", valorHora);
+
+	}
+}
