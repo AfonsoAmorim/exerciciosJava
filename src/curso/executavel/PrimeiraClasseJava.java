df7e566149cdc11a00b17eaa1cbc678d7210fc3a
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-05-18 21:20:35 -0300

    calcular salario

--- src/curso/executavel/PrimeiraClasseJava.java
+++ src/curso/executavel/PrimeiraClasseJava.java
@@ -1,26 +1,28 @@
 package curso.executavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 	
 	/* Main é um método auto executável em Java*/
 	public static void main(String[] args) {
-		System.out.println("IF e Else");
+		System.out.println("Hora de Trabalho");
 		
-		int num = 7;
-		int num1 = 150;
-		int soma = num+num1;
-		
-		
-		if(soma <= 100) {
-			System.out.println("O valor está entre 0 e 100");
-		}else if((soma >= 100)&& ((soma <= 200))){
-			System.out.println("A soma está entre 100 e 200");
+		int num = Integer.parseInt(JOptionPane.showInputDialog("Digite seu salário"));
+		int num1 = Integer.parseInt(JOptionPane.showInputDialog("Digite dias trabalhados"));
+		int valorHora = num/num1;
+		if(valorHora <= 100) {
+			System.out.println("O valor da sua hora de trabalho está entre 0 e R$ 100.");
+		}else if((valorHora > 100)&& ((valorHora <= 200))){
+			System.out.println("O valor da sua hora está entre R$100 e R$200.");
 		}else {
-			System.out.println("A soma é maior que 300");
+			System.out.println("O valor da sua hora é maior que R$ 300.");
 		}
 		
-		System.out.println("Soma de valores inteiros abaixo!!");
-		System.out.println(soma);
+		System.out.println("A sua hora vale R$" + valorHora);
+		
+		
+		
 	}
 	
 }
